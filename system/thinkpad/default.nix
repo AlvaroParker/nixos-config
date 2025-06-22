36998e4fc98197bdb0eacecfebb729b494a35e9f
Alvaro Parker <aparkerdf@protonmail.com>
2025-06-22 13:05:31 -0400

    Add steam to thinkpad config

--- system/thinkpad/default.nix
+++ system/thinkpad/default.nix
@@ -53,4 +53,15 @@
   environment.variables = {
     XKB_CONFIG_ROOT = "${pkgs.xkeyboard_config}/share/X11/xkb/";
   };
+
+  # Enable steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall =
+      true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall =
+      true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall =
+      true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
